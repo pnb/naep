36be0373dc30c93e22823ebc90b7ec5d4161a4da
pnb <pnigelb@gmail.com>
2019-07-08 15:38:10 -0500

    Small bug fix + new option
    Unsupervised correlated feature set splitting can now remove perfectly correlated features, since that was an issue in some datasets

--- misc_util.py
+++ misc_util.py
@@ -4,13 +4,16 @@ import numpy as np
 from tqdm import tqdm
 
 
-def uncorrelated_feature_sets(pandas_df, max_rho=.8, verbose=0):
+def uncorrelated_feature_sets(pandas_df, max_rho=.8, remove_perfect_corr=False, verbose=0):
     """Given a dataset with some features, return a list of lists, where each sublist is a set of
     feature names such that no pair of features are correlated more than `max_rho`.
 
     Args:
         pandas_df (pd.DataFrame): Dataset, where every column is assumed to be a numeric feature
         max_rho (float): Maximum allowable pairwise absolute correlation between two features
+        remove_perfect_corr (bool): If True, when a pair of features correlate perfectly (rho = 1),
+                                    remove one of them completely from consideration
+        verbose (int): Verbosity level
 
     Returns:
         list of lists: One or more sets of uncorrelated features
@@ -26,20 +29,25 @@ def uncorrelated_feature_sets(pandas_df, max_rho=.8, verbose=0):
                     rho.at[a, b] = 0
                 else:
                     rho.at[a, b] = rho.at[b, a] = stats.spearmanr(pandas_df[a], pandas_df[b])[0]
+    if rho.isnull().sum().sum() > 0:
+        raise ValueError('Correlation matrix had NaN values; check that there are no missing values'
+                         ' in inputs, and that each input feature has some variance')
 
     result = []
     current_set = list(pandas_df.columns)
     next_set = []
     while True:
         # Find maximum pairwise correlation to see if further splitting of feature set is needed
-        if rho.loc[current_set, current_set].max().max() > max_rho:
+        highest_corr = rho.loc[current_set, current_set].max().max()
+        if highest_corr > max_rho:
             a = rho.loc[current_set, current_set].max().idxmax()
             b = rho.loc[a, current_set].idxmax()
             if verbose > 1:
                 print(a, 'correlated with', b, 'rho =', rho.at[a, b])
             # Break ties based on which of the pair has higher mean correlation with other features
             to_remove = a if rho.loc[a, current_set].mean() > rho.loc[b, current_set].mean() else b
-            next_set.append(to_remove)
+            if highest_corr < 1 or not remove_perfect_corr:
+                next_set.append(to_remove)
             current_set.remove(to_remove)
         elif len(next_set) > 0:
             if verbose > 0:
@@ -58,5 +66,5 @@ def uncorrelated_feature_sets(pandas_df, max_rho=.8, verbose=0):
 
 if __name__ == '__main__':
     df = pd.DataFrame({'w': [2, 2, 3, 4, 5], 'x': [1, -2, 1, 3, 3], 'y': [5, 1, 3, 0, 1],
-                       'z': [1.1, -1, 1, 5, 5]})
-    print(uncorrelated_feature_sets(df, max_rho=.5, verbose=2))
+                       'z': [1.1, -1, 1, 5, 5], 'w2': [2, 2, 3, 4, 5]})
+    print(uncorrelated_feature_sets(df, max_rho=.5, verbose=2, remove_perfect_corr=True))
