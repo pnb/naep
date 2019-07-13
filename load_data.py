8fabeebc5bf680ac59fd3e40d12ad3b3859c763e
pnb <pnigelb@gmail.com>
2019-07-13 15:55:20 -0500

    Performance improvements for data loading

--- load_data.py
+++ load_data.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 
 
+df_cache = {}
+
+
 def _preprocess(df):
     df['time_unix'] = pd.to_datetime(df.EventTime).astype(np.int64) // 10 ** 6
     # First Text Change basically only happens in one item, and is redundant with the action before
@@ -22,37 +25,58 @@ def _preprocess(df):
 
 
 def train_full():
-    df = pd.read_csv('public_data/data_a_train.csv')
-    label_df = pd.read_csv('public_data/data_train_label.csv')
-    assert len(df.STUDENTID.unique()) == len(label_df.STUDENTID.unique())
-    gt = {p: l for p, l in label_df.values}
-    df['label'] = [gt[p] for p in df.STUDENTID.values]
-    return _preprocess(df)
+    if 'train_full' not in df_cache:
+        df = pd.read_csv('public_data/data_a_train.csv')
+        label_df = pd.read_csv('public_data/data_train_label.csv')
+        assert len(df.STUDENTID.unique()) == len(label_df.STUDENTID.unique())
+        gt = {p: l for p, l in label_df.values}
+        df['label'] = [gt[p] for p in df.STUDENTID.values]
+        df_cache['train_full'] = _preprocess(df)
+    return df_cache['train_full'].copy()
 
 
 def train_10m():
     # Return only first 10 minutes of data per participant
-    df = train_full()
-    start_unix_map = {p: v.time_unix.min() for p, v in df.groupby('STUDENTID')}
-    df['start_unix'] = [start_unix_map[p] for p in df.STUDENTID]
-    return df[df.time_unix < df.start_unix + 10 * 60 * 1000].drop(columns='start_unix')
+    if 'train_10m' not in df_cache:
+        df = train_full()
+        start_unix_map = {p: v.time_unix.min() for p, v in df.groupby('STUDENTID')}
+        df['start_unix'] = [start_unix_map[p] for p in df.STUDENTID]
+        df_cache['train_10m'] = df[df.time_unix < df.start_unix + 10 * 60 * 1000] \
+            .drop(columns='start_unix')
+    return df_cache['train_10m'].copy()
 
 
 def train_20m():
     # Return only first 20 minutes of data per participant
-    df = train_full()
-    start_unix_map = {p: v.time_unix.min() for p, v in df.groupby('STUDENTID')}
-    df['start_unix'] = [start_unix_map[p] for p in df.STUDENTID]
-    return df[df.time_unix < df.start_unix + 20 * 60 * 1000].drop(columns='start_unix')
+    if 'train_20m' not in df_cache:
+        df = train_full()
+        start_unix_map = {p: v.time_unix.min() for p, v in df.groupby('STUDENTID')}
+        df['start_unix'] = [start_unix_map[p] for p in df.STUDENTID]
+        df_cache['train_20m'] = df[df.time_unix < df.start_unix + 20 * 60 * 1000] \
+            .drop(columns='start_unix')
+    return df_cache['train_20m'].copy()
 
 
 def holdout_10m():
-    return _preprocess(pd.read_csv('public_data/data_a_hidden_10.csv'))
+    if 'holdout_10m' not in df_cache:
+        df_cache['holdout_10m'] = _preprocess(pd.read_csv('public_data/data_a_hidden_10.csv'))
+    return df_cache['holdout_10m'].copy()
 
 
 def holdout_20m():
-    return _preprocess(pd.read_csv('public_data/data_a_hidden_20.csv'))
+    if 'holdout_20m' not in df_cache:
+        df_cache['holdout_20m'] = _preprocess(pd.read_csv('public_data/data_a_hidden_20.csv'))
+    return df_cache['holdout_20m'].copy()
 
 
 def holdout_30m():
-    return _preprocess(pd.read_csv('public_data/data_a_hidden_30.csv'))
+    if 'holdout_30m' not in df_cache:
+        df_cache['holdout_30m'] = _preprocess(pd.read_csv('public_data/data_a_hidden_30.csv'))
+    return df_cache['holdout_30m'].copy()
+
+
+def all_unique_rows():
+    # Return all data, including training and holdout data, but not including overlapping subsets of
+    # the training data (10m, 20m)
+    return pd.concat([train_full(), holdout_10m(), holdout_20m(), holdout_30m()],
+                     ignore_index=True, sort=False)
