bae942134a110aafe89fb8571fabf89eb5fafb52
pnb <pnigelb@gmail.com>
2019-08-12 18:14:28 -0500

    8/12 submission
    Small change: adjusted predictions to match expected base rate

--- combine_predictions.py
+++ combine_predictions.py
@@ -1,11 +1,11 @@
 # Combine predictions from one or more models and put them in the correct submission format
 import re
+import os
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-from scipy import stats
 from scipy.spatial.distance import jensenshannon
+from sklearn import metrics
 
 
 VALIDITY_REGEX = r'^(\s*0*((0?\.[0-9]+)|(0\.?)|(1\.?)|(1\.0*))\s*,){1231}\s*0*((0?\.[0-9]+)|(0\.?)|(1\.?)|(1\.0*))\s*$'
@@ -18,10 +18,41 @@ model_fe = pd.read_csv('model_fe-0.csv')
 model_fe = model_fe[model_fe.holdout == 1]
 model_tsfresh = pd.read_csv('model_tsfresh.csv')
 model_featuretools = pd.read_csv('model_featuretools-0.csv')
-print('FE <=> TSFresh r =', np.corrcoef(model_fe.pred, model_tsfresh.pred)[0, 1])
-print('FE <=> TSFresh rho =', stats.spearmanr(model_fe.pred, model_tsfresh.pred)[0])
-print('FE <=> Featuretools r =', np.corrcoef(model_fe.pred, model_featuretools.pred)[0, 1])
-print('FE <=> Featuretools rho =', stats.spearmanr(model_fe.pred, model_featuretools.pred)[0])
+model_featuretools = model_featuretools[model_featuretools.holdout == 1]
+print('FE <=> TSFresh r =', model_fe.pred.corr(model_tsfresh.pred))
+print('FE <=> TSFresh rho =', model_fe.pred.corr(model_tsfresh.pred, method='spearman'))
+print('FE <=> Featuretools r =', model_fe.pred.corr(model_featuretools.pred))
+print('FE <=> Featuretools rho =', model_fe.pred.corr(model_featuretools.pred, method='spearman'))
+
+# TODO: This fusion approach does not work well for all models together, possibly because predictions are bad across folds?
+'''
+# Select the best models and fuse only those
+models = []
+for fname in sorted(os.listdir('.')):
+    if fname.startswith('model_featuretools') and fname.endswith('.csv'):
+        models.append(pd.read_csv(fname))
+for data_len in sorted(models[0].data_length.unique()):
+    votes = []
+    models_for_len = sorted([m[m.data_length == data_len] for m in models],
+                            key=lambda x: x.kappa_min.iloc[0], reverse=True)
+    print(['%.3f' % m.kappa_min.iloc[0] for m in models_for_len])
+    votes = [m for m in models_for_len  # Only keep models that are over 95% as good as the best
+             if m.kappa_min.iloc[0] > models_for_len[0].kappa_min.iloc[0] * .95]
+    print(len(votes))
+    fused = votes[0].pred * votes[0].kappa_min
+    for vote in votes[1:]:
+        fused += vote.pred * vote.kappa_min
+    fused /= sum(v.kappa_min.iloc[0] for v in votes)
+    gt = votes[0][votes[0].holdout == 0].label.astype(int)
+    print('Fused kappa:', data_len, metrics.cohen_kappa_score(gt, fused.loc[gt.index] > .5))
+exit()
+'''
+
+# Adjust predictions so predicted rate matches base rate for each data length
+for data_len in [10, 20, 30]:
+    len_df = model_fe[model_fe.data_length == data_len]
+    thresh = len_df.pred.quantile(1 - 0.6038961038961039)
+    model_fe.loc[len_df.index, 'pred'] = [v / 3 if v < thresh else v / 3 + .5 for v in len_df.pred]
 
 df = model_fe
 preds = ','.join(df.pred.astype(str))
