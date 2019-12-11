d2da48c00f43f353001898dcddddca84e5197978
pnb <pnigelb@gmail.com>
2019-12-11 07:43:58 -0600

    Remove old files

--- model_lstm.py
+++
@@ -1,3 +0,0 @@
-# Idea: there are 23695 student*problem sequences, which is maybe enough to build RNN models for
-# each sequence with the student's label as output.
-# Potential problem: different problems have different, non-generalizable patterns
